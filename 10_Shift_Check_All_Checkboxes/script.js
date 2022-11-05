a142ff3bea170cf8faba0406dcd47f0eb93866ab
Konstantinos K <koufogiannakiskon@gmail.com>
2022-11-05 15:37:40 +0200

    Check all checkboxes with Shift

---
+++ 10_Shift_Check_All_Checkboxes/script.js
@@ -0,0 +1,29 @@
+const checkboxes = document.querySelectorAll('.inbox input[type="checkbox"]');
+
+let lastChecked;
+
+function handleCheck(e) {
+    //Check if they had shift key down
+    //and that they are checking it
+    let inBetween = false;
+
+    if (e.shiftKey && this.checked) {
+        checkboxes.forEach(checkbox => {
+            console.log(checkbox);
+            if (checkbox === this || checkbox === lastChecked) {
+                inBetween = !inBetween;
+                console.log('Starting to check them in between!');
+            }
+
+            if (inBetween) {
+                checkbox.checked = true;
+            }
+        })
+    }
+
+    lastChecked = this;
+}
+
+
+
+checkboxes.forEach(checkbox => checkbox.addEventListener('click', handleCheck));
